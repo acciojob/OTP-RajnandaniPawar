7d64f034975d02ea62f5b2993ee825793fffdbc7
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-22 22:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,12 @@
 window.onload = function() {
     const codes = document.querySelectorAll('.code');
 
-    // Iterate through the inputs to add event listeners
-    codes.forEach((input, index) => {
-        input.addEventListener('input', (e) => {
-            if (e.data && e.data.charCodeAt(0) >= 48 && e.data.charCodeAt(0) <= 57) {
-                // If a number is typed, move to the next input
-                codes[index + 1]?.focus();
-            } else {
-                // If input is not a number, clear the input
-                input.value = '';
-            }
-        });
-        input.addEventListener('keydown', (e) => {
-            if (e.key === 'Backspace') {
-                // On backspace, move focus to the previous input
-                codes[index - 1]?.focus();
+    codes.forEach((code, idx) => {
+        code.addEventListener('keydown', (e) => {
+            if(e.key >= 0 && e.key <= 9) {
+                setTimeout(() => codes[idx + 1]?.focus(), 10);
+            } else if (e.key === "Backspace") {
+                setTimeout(() => codes[idx - 1]?.focus(), 10);
             }
         });
     });
